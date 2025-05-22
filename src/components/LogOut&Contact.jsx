6d6b43568e21b2a124af8eba1ad4563fa9878178
Zwe Let Yar <letyarzwe@gmail.com>
2025-05-22 12:25:46 +0630

    Contact to Developer Completed

--- src/components/LogOut&Contact.jsx
+++ src/components/LogOut&Contact.jsx
@@ -5,7 +5,13 @@ export default function LogOut() {
   let { logout } = useGoogleAuth();
   return (
     <div className="flex flex-col gap-3 items-center text-sm text-white">
-      <button className="bg-blue-600 rounded-full py-2 px-3  w-full cursor-pointer">
+      <button
+        className="bg-blue-600 rounded-full py-2 px-3  w-full cursor-pointer"
+        onClick={() => {
+          window.location.href =
+            "mailto:letyarzwe@gmail.com?subject=Feedback&body=Hi there,";
+        }}
+      >
         Contact to developer
       </button>
       <button
